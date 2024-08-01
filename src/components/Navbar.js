f3a266f6eccf2aff6dfdcb3cb042981acc08538a
Xavier <xnavarro@ucsd.edu>
2024-07-31 21:21:20 -0700

    add a placeholder button for cart and documentation for navbar files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
+/**
+ * Navbar component that renders the navigation bar.
+ * It includes links to different pages and a button.
+ */
 function Navbar() {
   return (
     <nav className="navbar">
+      {/* Left section of the navbar */}
       <div className="navbar-left">
+        {/* Link to the home page with brand name */}
         <Link to="/" className="navbar-brand">K-Express</Link>
       </div>
+      {/* Right section of the navbar */}
       <div className="navbar-right">
+        {/* Link to the home page */}
         <Link to="/" className="navbar-item">Menu</Link>
-        <Link to="/about" className="navbar-item">About Us</Link> {/* Update the Link */}
+        {/* Link to the about us page */}
+        <Link to="/about" className="navbar-item">About Us</Link>
+        {/* Link to the login page */}
         <Link to="/login" className="navbar-login">Login</Link>
+        {/* Link to the signup page */}
         <Link to="/signup" className="navbar-signup">Sign Up</Link>
+        {/* Button for the cart */}
+        <button className="cart">Cart</button>
       </div>
     </nav>
   );
