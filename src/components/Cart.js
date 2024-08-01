a6778e3f7d937d2d0893cc26ee0458af56e9de78
BryHong <b2hong@ucsd.edu>
2024-07-31 22:20:34 -0700

    add trashcan and addsub logic to moneyval

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FaTrash } from 'react-icons/fa';
 import './Cart.css';
 
 function Cart({ toggleCart }) {
@@ -18,6 +19,26 @@ function Cart({ toggleCart }) {
     ));
   };
 
+  const removeItem = (id) => {
+    setCartItems(cartItems.filter(item => item.id !== id));
+  };
+
+  const addTestItem = () => {
+    const newItem = {
+      id: Date.now(),
+      name: `Test Dish ${cartItems.length + 1}`,
+      price: +(Math.random() * 50 + 10).toFixed(2),
+      servings: Math.floor(Math.random() * 5) + 1,
+      quantity: 1,
+      image: 'path_to_placeholder_image.jpg'
+    };
+    setCartItems([...cartItems, newItem]);
+  };
+
+  const calculateTotalPrice = (item) => {
+    return (item.price * item.quantity).toFixed(2);
+  };
+
   return (
     <div className="cart">
       <button className="close-cart" onClick={toggleCart}>&times;</button>
@@ -28,18 +49,24 @@ function Cart({ toggleCart }) {
             <img src={item.image} alt={item.name} className="cart-item-image" />
             <div className="cart-item-details">
               <div className="cart-item-name">{item.name}</div>
-              <div className="cart-item-price">${item.price.toFixed(2)}</div>
+              <div className="cart-item-price">${calculateTotalPrice(item)}</div>
               <div className="cart-item-servings">{item.servings} servings</div>
             </div>
-            <div className="cart-item-quantity">
-              <button className="quantity-button" onClick={() => decrementQuantity(item.id)}>-</button>
-              <span className="quantity-value">{item.quantity}</span>
-              <button className="quantity-button" onClick={() => incrementQuantity(item.id)}>+</button>
+            <div className="cart-item-actions">
+              <button className="remove-item" onClick={() => removeItem(item.id)}>
+                <FaTrash />
+              </button>
+              <div className="cart-item-quantity">
+                <button className="quantity-button" onClick={() => decrementQuantity(item.id)}>-</button>
+                <span className="quantity-value">{item.quantity}</span>
+                <button className="quantity-button" onClick={() => incrementQuantity(item.id)}>+</button>
+              </div>
             </div>
           </li>
         ))}
       </ul>
       <button className="checkout-button">Checkout</button>
+      <button className="add-test-item-button" onClick={addTestItem}>Add Test Item</button>
     </div>
   );
 }
