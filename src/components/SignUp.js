cce411a2e92ad41cfdf7806046cf76b86e3ca483
Xavier <xnavarro@ucsd.edu>
2024-08-06 15:43:04 -0700

    documented changes for signup.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,20 +3,48 @@ import axios from 'axios';
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from "jwt-decode";
 
+
+/**
+ * SignUp Component
+ * 
+ * This component renders a sign-up form with fields for email, password, and password confirmation.
+ * It also includes a Google login button for authentication.
+ */
 function SignUp() {
+  // State variables to store email, password, password confirmation, and sign-up status
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const [isSignedUp, setIsSignedUp] = useState(false);
 
-  useEffect(() => {
+
+
+  /* THIS SHOULD BE USED FOR LOGIN AND NOT FOR SIGN UP */ 
+
+  /**
+   * useEffect Hook
+   * 
+   * This hook runs once when the component mounts. It retrieves the 'isSignedUp' status from local storage,
+   * logs the status to the console for debugging purposes, and updates the state if the user is signed up.
+   */
+  /*useEffect(() => {
+    // Retrieve the 'isSignedUp' status from local storage
     const signedUpStatus = localStorage.getItem('isSignedUp');
+    
+    // Log the signed-up status to the console for debugging purposes
     console.log('Signed up status on load:', signedUpStatus);
+    
+    // If the signed-up status is 'true', update the state to reflect that the user is signed up
     if (signedUpStatus === 'true') {
       setIsSignedUp(true);
     }
-  }, []);
+  }, []);*/
 
+  /**
+   * Handles form submission
+   * 
+   * @param {Event} e - The form submission event
+   */
   const handleSubmit = async (e) => {
     e.preventDefault();
     const userData = {
@@ -41,6 +69,11 @@ function SignUp() {
     }
   };
 
+  /**
+   * Handles Google login success
+   * 
+   * @param {Object} response - The response object from Google login
+   */
   const handleGoogleSuccess = async (credentialResponse) => {
     try {
       const decodedToken = jwtDecode(credentialResponse.credential);
@@ -60,11 +93,21 @@ function SignUp() {
     }
   };
 
+  /**
+   * JSX Return Statement
+   * 
+   * This section of the code renders the UI based on the user's sign-up status.
+   * If the user is signed up, it displays a success message.
+   * Otherwise, it renders a sign-up form with fields for email, password, and password confirmation,
+   * along with a Google login button.
+   */
   return (
     <div>
       {isSignedUp ? (
-        <h1>Login Successful</h1>
+        // Display success message if the user is signed up
+        <h1>Account Created</h1>
       ) : (
+        // Render the sign-up form if the user is not signed up
         <form onSubmit={handleSubmit}>
           <input
             type="email"
