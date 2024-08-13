e673e2a8a4d48645c83ab4c69ebdf52b1f13ccbe
BryHong <b2hong@ucsd.edu>
2024-08-12 22:45:31 -0700

    fixed errors firebase

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -23,5 +23,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
+export { auth, db };
