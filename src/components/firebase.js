2a2e79c41a92215e8eafdc65ed06843bc9a31a88
BryHong <b2hong@ucsd.edu>
2024-08-12 22:43:12 -0700

    firebase info keys

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,20 +1,27 @@
-// src/firebase.js
-import { initializeApp } from 'firebase/app';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // Your Firebase configuration object
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
+  apiKey: "AIzaSyA_ExaBGVmoCVghAoP57EUSOX15m1iIjyU",
+  authDomain: "real-k-express.firebaseapp.com",
+  databaseURL: "https://real-k-express-default-rtdb.firebaseio.com",
   projectId: "real-k-express",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  storageBucket: "real-k-express.appspot.com",
+  messagingSenderId: "197268968087",
+  appId: "1:197268968087:web:493a1ee91c04f3487e4fd2",
+  measurementId: "G-GZFPNRYHVF"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
+const analytics = getAnalytics(app);
